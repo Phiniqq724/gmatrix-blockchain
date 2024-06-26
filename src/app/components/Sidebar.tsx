1207d32ea2d75ec2cf4056d726c1e54f980249f5
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-06-26 11:59:33 +0700

    tes

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -117,7 +117,7 @@ export const AdminSidebar = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link href={"/admin/userData"} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-blue-600 duration-500 group w-full">
+                  <Link href="/admin/userData" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-blue-600 duration-500 group w-full">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         d="M20 21C20 19.6044 20 18.9067 19.8278 18.3389C19.44 17.0605 18.4395 16.06 17.1611 15.6722C16.5933 15.5 15.8956 15.5 14.5 15.5H9.5C8.10444 15.5 7.40665 15.5 6.83886 15.6722C5.56045 16.06 4.56004 17.0605 4.17224 18.3389C4 18.9067 4 19.6044 4 21M16.5 7.5C16.5 9.98528 14.4853 12 12 12C9.51472 12 7.5 9.98528 7.5 7.5C7.5 5.01472 9.51472 3 12 3C14.4853 3 16.5 5.01472 16.5 7.5Z"
@@ -131,7 +131,7 @@ export const AdminSidebar = () => {
                     <span className="flex-1 ms-3 whitespace-nowrap text-lg text-start">Users</span>
                   </Link>
                 </li>
-                {modal && (
+                {/* {modal && (
                   <>
                     <li>
                       <Link href="/admin/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-blue-600 duration-500 group">
@@ -152,7 +152,7 @@ export const AdminSidebar = () => {
                       </a>
                     </li>
                   </>
-                )}
+                )} */}
                 <li>
                   <Link href="/admin/bookData" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-blue-600 duration-500 group">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
