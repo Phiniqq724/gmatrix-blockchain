31394ce1b18a8a7253e248dad3cd1ac43f5639fb
phiniqq724 <fahrellnew@gmail.com>
2024-06-26 10:04:18 +0700

    chore: Refactor Searchbar component and add lazy loading for images in UserDashboard page

---
+++ src/app/components/Bookcard.tsx
@@ -0,0 +1,38 @@
+import {
+  StaticImageData,
+  StaticImport,
+} from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import { ReactElement } from "react";
+
+interface BookCardProops {
+  src?:
+    | StaticImageData
+    | undefined
+    | string
+    | StaticImport
+    | ReactElement
+    | any;
+  title?: string;
+  author?: string;
+  className?: string;
+}
+
+export default function BookCard({
+  src,
+  title,
+  author,
+  className,
+}: BookCardProops) {
+  return (
+    <main className={className}>
+      <div>
+        <Image src={src} alt="image" />
+      </div>
+      <div className="font-semibold text-blue-400 text-center text-xl">
+        {title}
+      </div>
+      <div className="text-center text-lg">{author}</div>
+    </main>
+  );
+}
